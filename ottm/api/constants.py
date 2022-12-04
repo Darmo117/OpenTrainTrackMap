cbfe3f5c646eb9e8d39fff60653f123bd4db3abc
Darmo117 <darmo117@gmail.com>
2022-12-04 20:29:23 +0100

    add type hints

--- ottm/api/constants.py
+++ ottm/api/constants.py
@@ -2,12 +2,12 @@ from .. import util
 
 OBJECT_CREATED = 'create'
 OBJECT_DELETED = 'delete'
-OBJECT_EDIT_ACTIONS = util.gather_globals('^OBJECT_', str)
+OBJECT_EDIT_ACTIONS: tuple[str, ...] = util.gather_globals('^OBJECT_', str)
 
 RELATION_CREATED = 'create'
 RELATION_MODIFIED = 'modify'
 RELATION_DELETED = 'delete'
-RELATION_EDIT_ACTIONS = util.gather_globals('^RELATION_', str)
+RELATION_EDIT_ACTIONS: tuple[str, ...] = util.gather_globals('^RELATION_', str)
 
 PROPERTY_TYPE = 'type'
 PROPERTY_LOCALIZED = 'localized'
@@ -17,4 +17,4 @@ PROPERTY_FLOAT = 'float'
 PROPERTY_BOOLEAN = 'boolean'
 PROPERTY_UNIT = 'unit'
 PROPERTY_DATE_INTERVAL = 'date_interval'
-PROPERTY_TYPES = util.gather_globals('^PROPERTY_', str)
+PROPERTY_TYPES: tuple[str, ...] = util.gather_globals('^PROPERTY_', str)
