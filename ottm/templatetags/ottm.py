0b9a8342dd17451db826c7f56ea250eb741248b2
Darmo117 <darmo117@gmail.com>
2022-12-17 21:39:23 +0100

    add icons to user accounts

--- ottm/templatetags/ottm.py
+++ ottm/templatetags/ottm.py
@@ -4,9 +4,10 @@ import typing as _typ
 
 import django.template as _dj_template
 import django.utils.safestring as _dj_safe
-import pytz
+import pytz as _pytz
 
 from .. import page_handlers as _ph
+from ..api import auth as _auth
 
 register = _dj_template.Library()
 TemplateContext = dict[str, _typ.Any]
@@ -36,7 +37,7 @@ def ottm_format_time(context: TemplateContext, date: _dt.datetime, timezone: str
     """
     ottm_context: _ph.PageContext = context['context']
     if timezone:
-        tz = pytz.timezone(timezone)
+        tz = _pytz.timezone(timezone)
     else:
         tz = ottm_context.user.prefered_timezone
     date = date.astimezone(tz)
@@ -55,7 +56,7 @@ def ottm_format_date(context: TemplateContext, date: _dt.datetime, timezone: str
     """
     ottm_context: _ph.PageContext = context['context']
     if timezone:
-        tz = pytz.timezone(timezone)
+        tz = _pytz.timezone(timezone)
     else:
         tz = ottm_context.user.prefered_timezone
     date = date.astimezone(tz)
@@ -76,3 +77,29 @@ def ottm_format_number(context: TemplateContext, n: int | float, value_only: boo
     ottm_context: _ph.PageContext = context['context']
     s = ottm_context.language.format_number(n)
     return _dj_safe.mark_safe(f'<data value="{n}">{s}</data>') if not value_only else s
+
+
+@register.simple_tag(takes_context=True)
+def ottm_user_type_icon(context: TemplateContext, username: str) -> str:
+    """Return the account type icon for the user that matches the given username.
+
+    :param context: Page context.
+    :param username: Username of the user.
+    :return: The icon.
+    """
+    ottm_context: _ph.PageContext = context['context']
+    user = _auth.get_user_from_name(username)
+    if not user:
+        key = 'page.user_profile.statistics.status_non_existent'
+        icon = 'diameter-variant'
+    elif not user.is_authenticated:
+        key = 'page.user_profile.statistics.status_anonymous'
+        icon = 'incognito'
+    elif user.is_bot:
+        key = 'page.user_profile.statistics.status_bot'
+        icon = 'robot-happy-outline'
+    else:
+        key = 'page.user_profile.statistics.status_user'
+        icon = 'account'
+    tooltip = ottm_context.language.translate(key)
+    return _dj_safe.mark_safe(f'<span class="mdi mdi-{icon}" title="{tooltip}"></span>')
