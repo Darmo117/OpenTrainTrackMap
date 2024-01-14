78cff7540f3c6da47b8600aa6b728bd62cd02995
Darmo117 <darmo117@gmail.com>
2024-01-14 16:27:26 +0100

    remove mapboxgl-draw from dependencies; remove old map editor files

--- js/modules/map/snap/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as SnapPointMode} from "./modes/snap_point";
-export {default as SnapLineMode} from "./modes/snap_line";
-export {default as SnapPolygonMode} from "./modes/snap_polygon";
-export {default as SnapDirectSelect} from "./modes/snap_direct_select";
-
-export {default as SnapModeDrawStyles} from "./utils/customDrawStyles";
--- js/modules/map/snap/modes/patch.ts
+++
@@ -1,6 +0,0 @@
-import {DrawCustomMode} from "@mapbox/mapbox-gl-draw";
-
-export interface DrawCustomModeWithContext<CustomModeState = any, CustomModeOptions = any>
-  extends DrawCustomMode<CustomModeState, CustomModeOptions> {
-  _ctx: any; // Expose internal property
-}
