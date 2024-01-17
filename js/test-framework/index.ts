55639c18072b07c0b8e5e9844af359333b1f41ed
Darmo117 <darmo117@gmail.com>
2024-01-17 16:50:10 +0100

    doc for test-framework/index.ts

--- js/test-framework/index.ts
+++ js/test-framework/index.ts
@@ -1,9 +1,15 @@
+/**
+ * The structure of a test context.
+ */
 type Context = {
   type: "context";
   name: string;
   elements: Context[] | Test[];
 };
 
+/**
+ * The structure of a test definition.
+ */
 type Test = {
   type: "test";
   description: string;
@@ -11,13 +17,30 @@ type Test = {
   disabled: boolean;
 };
 
+/**
+ * Options used by the {@link doTests} function.
+ */
 export const options = {
+  /**
+   * Whether to show tests that passed.
+   */
   showPassed: true,
+  /**
+   * Whether to show tests that were ignoredd.
+   */
   showIgnored: false,
+  /**
+   * Whether to show the error messages of tests that failed.
+   */
   showErrorMessages: true,
 };
 
-export function doTests(...contexts: Context[]) {
+/**
+ * Perform the tests of all passed contexts.
+ * When all tests have been executed, the number of passed, ignored and failed tests is printed.
+ * @param contexts The context to execute the tests of.
+ */
+export function doTests(...contexts: Context[]): void {
   let total = 0;
   let passed = 0;
   let failed = 0;
@@ -40,6 +63,11 @@ export function doTests(...contexts: Context[]) {
   }
   printColor(message, color);
 
+  /**
+   * Recursively execute the given contexts/tests.
+   * @param contexts The array of contexts/tests to execute.
+   * @param depth The current depth in the context tree.
+   */
   function tests(contexts: Context[] | Test[], depth: number = 0): void {
     const indent = "  ".repeat(depth);
     for (const e of contexts) {
@@ -74,6 +102,12 @@ export function doTests(...contexts: Context[]) {
   }
 }
 
+/**
+ * Define a context for a set of tests or sub-contexts.
+ * The context’s name will be shown before any of its tests or sub-contexts are executed.
+ * @param name The context’s name.
+ * @param tests The set of tests or sub-contexts for this context.
+ */
 export function describe(name: string, ...tests: Test[] | Context[]): Context {
   return {
     type: "context",
@@ -82,6 +116,12 @@ export function describe(name: string, ...tests: Test[] | Context[]): Context {
   }
 }
 
+/**
+ * Define a test with a specific description and action to perform.
+ * @param description The test’s description that will be shown in the results.
+ * @param action The function to execute. It must throw an AssertionError if the test fails.
+ * @param disable Optional. Whether to disable this test.
+ */
 export function test(description: string, action: () => void, disable?: boolean): Test {
   return {
     type: "test",
