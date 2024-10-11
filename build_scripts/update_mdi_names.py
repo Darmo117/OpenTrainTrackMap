6032560b3182c6e252cbf8ffb3db929313879234
Darmo117 <darmo117@gmail.com>
2024-10-11 22:13:56 +0200

    refactor update_imagery_sources.py

--- build_scripts/update_mdi_names.py
+++ build_scripts/update_mdi_names.py
@@ -7,60 +7,63 @@ import re
 
 import gitdir
 
-mdi_version_pattern = re.compile(r'\$mdi-version:\s*"([\d.]+)"')
-name_pattern = re.compile(r'^\s+(".+"):')
 
-raw_files_root = pathlib.Path('mdi_names/raw_files')
-scss_file_name = '_variables.scss'
-scss_file = raw_files_root / 'scss' / scss_file_name
-output_ts_file = pathlib.Path('../js/modules/map/controls/mdi.ts')
-output_static_dir = pathlib.Path('../ottm/static/ottm/libs/mdi')
+def main():
+    mdi_version_pattern = re.compile(r'\$mdi-version:\s*"([\d.]+)"')
+    name_pattern = re.compile(r'^\s+(".+"):')
 
-# Download SCSS file
-gitdir.download(
-    'https://github.com/Templarian/MaterialDesign-Webfont/tree/master/scss/',
-    raw_files_root,
-    # Only download the file we are interested in
-    lambda file_path: file_path.name == scss_file_name
-)
+    raw_files_root = pathlib.Path('mdi_names/raw_files')
+    scss_file_name = '_variables.scss'
+    scss_file = raw_files_root / 'scss' / scss_file_name
+    output_ts_file = pathlib.Path('../js/modules/map/controls/mdi.ts')
+    output_static_dir = pathlib.Path('../ottm/static/ottm/libs/mdi')
 
+    # Download SCSS file
+    gitdir.download(
+        'https://github.com/Templarian/MaterialDesign-Webfont/tree/master/scss/',
+        raw_files_root,
+        # Only download the file we are interested in
+        lambda file_path: file_path.name == scss_file_name
+    )
 
-def delete_contents(root: pathlib.Path):
-    for file in root.glob('*'):
-        if file.is_dir():
-            delete_contents(file)
-            file.rmdir()
-        else:
-            file.unlink()
+    def delete_contents(root: pathlib.Path):
+        for file in root.glob('*'):
+            if file.is_dir():
+                delete_contents(file)
+                file.rmdir()
+            else:
+                file.unlink()
 
+    delete_contents(output_static_dir)
 
-delete_contents(output_static_dir)
+    # Download CSS and font files
+    gitdir.download(
+        'https://github.com/Templarian/MaterialDesign-Webfont/tree/master/',
+        output_static_dir,
+        # Only download the file we are interested in
+        lambda file_path: file_path.parts[0] in ('css', 'fonts')
+    )
 
-# Download CSS and font files
-gitdir.download(
-    'https://github.com/Templarian/MaterialDesign-Webfont/tree/master/',
-    output_static_dir,
-    # Only download the file we are interested in
-    lambda file_path: file_path.parts[0] in ('css', 'fonts')
-)
+    names = []
+    version = None
+    with scss_file.open() as f:
+        for line in f.readlines():
+            if m := name_pattern.match(line):
+                names.append(m.group(1))
+            elif m := mdi_version_pattern.match(line):
+                version = m.group(1)
+    entries = '\n  | '.join(names)
 
-names = []
-version = None
-with scss_file.open() as f:
-    for line in f.readlines():
-        if m := name_pattern.match(line):
-            names.append(m.group(1))
-        elif m := mdi_version_pattern.match(line):
-            version = m.group(1)
-entries = '\n    | '.join(names)
-
-with output_ts_file.open(mode='w') as f:
-    f.write(f"""\
+    with output_ts_file.open(mode='w') as f:
+        f.write(f"""\
 // This file is generated from MDI’s SCSS sources, please do not edit manually!
 // MaterialDesign Icons v{version}
 type MDIcon =
-    | {entries}
-    ;
+  | {entries};
 
 export default MDIcon;
 """)
+
+
+if __name__ == '__main__':
+    main()
