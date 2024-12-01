cfe6424fabda20258049c2999af132f4fdda2b3b
Darmo117 <darmo117@gmail.com>
2024-12-01 23:50:31 +0100

    remove now unused patch_sources.py

--- js-sources/patch_sources.py
+++
@@ -1,6 +0,0 @@
-print('Patching generated bundle-index.css…')
-with open('../ottm/static/ottm/bundle-index.css', mode='r+') as f:
-    output = f.read().replace('/bundle-materialdesignicons-webfont', '/static/ottm/bundle-materialdesignicons-webfont')
-    f.seek(0)
-    f.write(output)
-    f.truncate()
